e9aa1cd84365cb26ad9a0292a16b0e167a77024d
DreDreDreDre <andrejar34@gmail.com>
2021-09-09 17:31:44 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+import java.util.Date;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int year;
+        int currentAge;
+        int retiringAge;
+        int yearsLeft;
+
+        String currentAgeIn;
+        String retiringAgeIn;
+
+        Date date = new Date();
+        Scanner sc = new Scanner(System.in);
+        year = date.getYear() + 1900;
+
+        System.out.print("What is your current age? ");
+        currentAgeIn = sc.nextLine();
+        currentAge = Integer.valueOf(currentAgeIn);
+
+        System.out.print("At what age would you like to retire? ");
+        retiringAgeIn = sc.nextLine();
+        retiringAge = Integer.valueOf(retiringAgeIn);
+
+        yearsLeft = retiringAge - currentAge;
+
+        System.out.println("You have " + yearsLeft + " years left until you can retire.");
+        System.out.println("It's " + year + ", so you can retire in " + (year + yearsLeft));
+    }
+}
