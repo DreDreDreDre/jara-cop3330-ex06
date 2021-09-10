25175299dfc436e7414824a606dd825a7b0d6b76
DreDreDreDre <andrejar34@gmail.com>
2021-09-09 21:29:56 -0400

    Added header

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,9 @@
 package org.example;
 import java.util.Date;
 import java.util.Scanner;
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Andre Jara
  */
 public class App 
 {
